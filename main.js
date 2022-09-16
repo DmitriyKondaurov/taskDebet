929ebf5a1765e788c8931669b03a1d973112eab2
Dmytro Kondaurov <invisibl@ukr.net>
2022-09-17 00:50:37 +0300

    get and fill random height/width in tables

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const fillButton = document.querySelector('[data-fill-btn]');
 const runButton = document.querySelector('[data-run-btn]');
 const widthTableEl = document.querySelector('[data-table-width]');
 const heightTableEl = document.querySelector('[data-table-height]');
+let rectanglesList = [];
 
 const addTableEl = (rowNumber, table) => {
   const newTrEl = document.createElement('tr');
@@ -9,15 +10,40 @@ const addTableEl = (rowNumber, table) => {
   for (let i = 0; i < 5; i++) {
     const indexTdEl = newTdEl.cloneNode(true);
     const valueTdEl = newTdEl.cloneNode(true);
-    indexTdEl.textContent = rowNumber * 5 + i;
+    indexTdEl.textContent = (rowNumber * 5 + i).toString();
+    // indexTdEl.textContent = (rowNumber * 5 + i).toString();
     newTrEl.appendChild(indexTdEl);
     newTrEl.appendChild(valueTdEl);
+
   }
-  document.body.querySelector(`[${table}]`).append(newTrEl);
+  table.append(newTrEl);
 }
 
-for (let i = 0; i < 20; i++){
-  addTableEl(i, 'data-table-height');
-  addTableEl(i, 'data-table-width');
+const buildTables = () => {
+  for (let i = 0; i < 20; i++){
+    addTableEl(i, widthTableEl);
+    addTableEl(i, heightTableEl);
+  }
 }
 
+const getRandom = () => Math.floor(Math.random() * 99) + 1;
+
+const fillArrays = () => {
+  for (let i = 0; i < 100; i++){
+    const rectangle = {};
+    rectangle.height = getRandom();
+    rectangle.width = getRandom();
+    rectanglesList.push(rectangle);
+    fillHtmlTables(i);
+  }
+  console.log(rectanglesList)
+}
+
+const fillHtmlTables = (index) => {
+  widthTableEl.children[Math.floor(index / 5)].children[index % 5 * 2 + 1].textContent = rectanglesList[index].width;
+  heightTableEl.children[Math.floor(index / 5)].children[index % 5 * 2 + 1].textContent = rectanglesList[index].height;
+}
+
+buildTables();
+
+fillButton.addEventListener('click', fillArrays);
