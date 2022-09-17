29b8dae848a7256ab053af95544e08b515d5c668
Dmytro Kondaurov <invisibl@ukr.net>
2022-09-17 03:42:21 +0300

    done logic

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+const recWidthsList = []
+const recHeightsList = []
 const clearButton = document.querySelector('[data-clear-btn]');
 const fillButton = document.querySelector('[data-fill-btn]');
 const runButton = document.querySelector('[data-run-btn]');
+const widthBiggerEl = document.querySelector('[data-width]');
+const heightBiggerEl = document.querySelector('[data-height]');
+const longestDiagonalEl = document.querySelector('[data-diagonal]');
 const widthTableEl = document.querySelector('[data-table-width]');
 const heightTableEl = document.querySelector('[data-table-height]');
-let rectanglesList = [];
-
 
 const addTableEl = (rowNumber, table) => {
   const newTrEl = document.createElement('tr');
@@ -13,7 +16,6 @@ const addTableEl = (rowNumber, table) => {
     const indexTdEl = newTdEl.cloneNode(true);
     const valueTdEl = newTdEl.cloneNode(true);
     indexTdEl.textContent = (rowNumber * 5 + i).toString();
-    // indexTdEl.textContent = (rowNumber * 5 + i).toString();
     newTrEl.appendChild(indexTdEl);
     newTrEl.appendChild(valueTdEl);
 
@@ -35,21 +37,60 @@ const fillArrays = () => {
     const rectangle = {};
     rectangle.height = getRandom();
     rectangle.width = getRandom();
-    rectanglesList.push(rectangle);
+    recWidthsList.push(rectangle.width);
+    recHeightsList.push(rectangle.height);
     fillHtmlTables(i);
   }
-  console.log(rectanglesList)
 }
 
 const fillHtmlTables = (index) => {
-  widthTableEl.children[Math.floor(index / 5)].children[index % 5 * 2 + 1].textContent = rectanglesList[index].width;
-  heightTableEl.children[Math.floor(index / 5)].children[index % 5 * 2 + 1].textContent = rectanglesList[index].height;
+  widthTableEl.children[Math.floor(index / 5)].children[index % 5 * 2 + 1].textContent = recWidthsList[index];
+  heightTableEl.children[Math.floor(index / 5)].children[index % 5 * 2 + 1].textContent = recHeightsList[index];
 }
 
-buildTables();
+const calculate = (widthArray, heightArray) => {
+
+  const rectanglesList = widthArray.map((element, index) => {
+    const height = heightArray[index];
+    const width = element;
+    const rectangleNumber = index;
+    return {rectangleNumber, height, width}
+  })
+
+  const widthBigger = rectanglesList
+    .filter((rectangle) => rectangle.width > rectangle.height)
+    .length
+
+  const heightBigger = rectanglesList
+    .filter((rectangle) => rectangle.width < rectangle.height)
+    .length
+
+  // const equal = rectanglesList.filter((rectangle) => rectangle.width === rectangle.height)
+  // console.log(rectanglesList);
+
+  const longestDiagonal = rectanglesList.reduce((acc, curr) => {
+    const diagonal = Math.sqrt((curr.height * curr.height) + (curr.width * curr.width));
+    if (acc < diagonal) {
+    return curr.rectangleNumber;
+    } else return acc;
+  },0)
+
+  widthBiggerEl.setAttribute('value', widthBigger);
+  heightBiggerEl.setAttribute ('value', heightBigger);
+  longestDiagonalEl.setAttribute ('value', longestDiagonal);
+}
+
 
 fillButton.addEventListener('click', fillArrays);
+
+runButton.addEventListener('click', () => {
+  if (recWidthsList.length > 1 && recHeightsList.length > 1) {
+    return calculate(recWidthsList, recHeightsList);
+  } else alert('Fill arrays first please!')
+});
 
 clearButton.addEventListener('click', () => {
   document.location.reload();
 })
+
+buildTables();
